07f04ec2043c1cf5e93ef90dbcd04005a6c3828e
pablo360able <pablo360.repiuk@gmail.com>
2025-10-14 14:02:14 -0400

    Created Power Up

---
+++ src/main/java/theaugment/powers/PowerUpPower.java
@@ -0,0 +1,44 @@
+package theaugment.powers;
+
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+import static theaugment.TheAugmentMod.makeID;
+
+public class PowerUpPower extends BasePower {
+    public static final String POWER_ID = makeID(PowerUpPower.class.getSimpleName());
+    private static final AbstractPower.PowerType TYPE = AbstractPower.PowerType.BUFF;
+    private static final boolean TURN_BASED = false;
+
+    public PowerUpPower(AbstractCreature owner) {
+        super(POWER_ID, TYPE, TURN_BASED, owner, -1);
+        this.description = DESCRIPTIONS[0];
+    }
+
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[1];
+    }
+
+    public void onCardDraw(AbstractCard card) {
+        if (card.type == AbstractCard.CardType.POWER) {
+            card.setCostForTurn(-9);
+        }
+
+    }
+
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (card.type == AbstractCard.CardType.SKILL) {
+            this.flash();
+            action.exhaustCard = true;
+        }
+
+    }
+
+    public void atStartOfTurn() {
+        this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, POWER_ID));
+    }
+}
