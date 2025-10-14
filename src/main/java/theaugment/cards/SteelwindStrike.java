61d85d8cb56a9e12cbfa88823d21dd092ad0f0e5
pablo360able <pablo360.repiuk@gmail.com>
2025-10-14 15:08:27 -0400

    Created Steelwind Strike

---
+++ src/main/java/theaugment/cards/SteelwindStrike.java
@@ -0,0 +1,51 @@
+package theaugment.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theaugment.character.Augment;
+import theaugment.util.CardStats;
+
+public class SteelwindStrike extends BaseCard {
+    public static final String ID = makeID(SteelwindStrike.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            Augment.Meta.CARD_COLOR,
+            CardType.ATTACK,
+            CardRarity.BASIC,
+            CardTarget.ENEMY,
+            1
+    );
+    private static final int DAMAGE = 3;
+    private static final int UPG_DAMAGE = 0;
+    private static final int MAGIC = 3;
+    private static final int UPG_MAGIC = 0;
+
+    public SteelwindStrike() {
+        super(ID, info);
+
+        setDamage(DAMAGE, UPG_DAMAGE);
+        setMagic(MAGIC, UPG_MAGIC);
+
+        tags.add(CardTags.STRIKE);
+    }
+
+    @Override
+    public void upgrade() {
+        this.baseCost = 0;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for(int i = 0; i < this.magicNumber; ++i) {
+            this.addToBot(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        }    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new SteelwindStrike();
+    }
+}
