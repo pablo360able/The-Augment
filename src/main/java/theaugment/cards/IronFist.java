42f13aa96abccb6a1238ca34552b9405366ab714
pablo360able <pablo360.repiuk@gmail.com>
2025-10-16 16:02:13 -0400

    Fixed cost for Future Strike

--- src/main/java/theaugment/cards/IronFist.java
+++ src/main/java/theaugment/cards/IronFist.java
@@ -1,31 +1,29 @@
 package theaugment.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.utility.ScryAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.FocusPower;
 import theaugment.actions.BloodAction;
 import theaugment.character.Augment;
 import theaugment.util.CardStats;
 
-public class FutureStrike extends BaseCard {
-    public static final String ID = makeID(FutureStrike.class.getSimpleName());
+public class IronFist extends BaseCard {
+    public static final String ID = makeID(IronFist.class.getSimpleName());
     private static final CardStats info = new CardStats(
             Augment.Meta.CARD_COLOR,
             CardType.ATTACK,
             CardRarity.COMMON,
             CardTarget.ENEMY,
-            1
+            2
     );
     private static final int DAMAGE = 5;
     private static final int UPG_DAMAGE = 3;
 
-    public FutureStrike() {
+    public IronFist() {
         super(ID, info);
 
         setDamage(DAMAGE, UPG_DAMAGE);
@@ -41,6 +39,6 @@ public class FutureStrike extends BaseCard {
 
     @Override
     public AbstractCard makeCopy() { //Optional
-        return new FutureStrike();
+        return new IronFist();
     }
 }
