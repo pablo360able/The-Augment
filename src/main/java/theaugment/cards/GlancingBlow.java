48a422516cfd656f3a7c75b323bd97387ff87fcf
pablo360able <pablo360.repiuk@gmail.com>
2025-10-16 16:26:51 -0400

    Added Glancing Blow

---
+++ src/main/java/theaugment/cards/GlancingBlow.java
@@ -0,0 +1,42 @@
+package theaugment.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theaugment.actions.BloodAction;
+import theaugment.character.Augment;
+import theaugment.util.CardStats;
+
+public class GlancingBlow extends BaseCard {
+    public static final String ID = makeID(GlancingBlow.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            Augment.Meta.CARD_COLOR,
+            CardType.ATTACK,
+            CardRarity.COMMON,
+            CardTarget.ENEMY,
+            0
+    );
+    private static final int DAMAGE = 5;
+    private static final int UPG_DAMAGE = 3;
+
+    public GlancingBlow() {
+        super(ID, info);
+
+        setDamage(DAMAGE, UPG_DAMAGE);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+        addToBot(new BloodAction(m, p, new DrawCardAction(1)));
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new GlancingBlow();
+    }
+}
