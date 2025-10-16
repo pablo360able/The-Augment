c3c5e268cd35918583732729fe2760d42c79cede
pablo360able <pablo360.repiuk@gmail.com>
2025-10-16 16:08:12 -0400

    Added Iron Fist, although it doesn't currently work

--- src/main/java/theaugment/cards/IronFist.java
+++ src/main/java/theaugment/cards/IronFist.java
@@ -1,12 +1,14 @@
 package theaugment.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.utility.ScryAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.NextTurnBlockPower;
 import theaugment.actions.BloodAction;
 import theaugment.character.Augment;
 import theaugment.util.CardStats;
@@ -20,21 +22,19 @@ public class IronFist extends BaseCard {
             CardTarget.ENEMY,
             2
     );
-    private static final int DAMAGE = 5;
-    private static final int UPG_DAMAGE = 3;
+    private static final int DAMAGE = 16;
+    private static final int UPG_DAMAGE = 5;
 
     public IronFist() {
         super(ID, info);
 
         setDamage(DAMAGE, UPG_DAMAGE);
-
-        tags.add(CardTags.STRIKE);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
-        addToBot(new BloodAction(m, p, new ScryAction(0), true));
+        addToBot(new BloodAction(m, p, new ApplyPowerAction(p, p, new NextTurnBlockPower(p, 0)), true));
     }
 
     @Override
