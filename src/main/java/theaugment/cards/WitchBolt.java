1af88e89e6ecd392a89ffcce848be3ee1a737f4d
pablo360able <pablo360.repiuk@gmail.com>
2025-10-16 21:41:28 -0400

    Added Witch Bolt

--- src/main/java/theaugment/cards/WitchBolt.java
+++ src/main/java/theaugment/cards/WitchBolt.java
@@ -2,13 +2,16 @@ package theaugment.cards;
 
 import com.evacipated.cardcrawl.mod.stslib.damagemods.DamageModifierManager;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction;
+import com.megacrit.cardcrawl.actions.common.*;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.orbs.AbstractOrb;
+import com.megacrit.cardcrawl.powers.StrengthPower;
 import modifiers.MagicAttack;
+import theaugment.actions.BloodAction;
 import theaugment.orbs.Aether;
 import theaugment.util.CardStats;
 
@@ -21,33 +24,22 @@ public class WitchBolt extends BaseCard {
             CardTarget.ENEMY,
             0
     );
-    private static final int DAMAGE = 7;
-    private static final int UPG_DAMAGE = 2;
+    private static final int DAMAGE = 12;
+    private static final int UPG_DAMAGE = 3;
 
     public WitchBolt() {
         super(ID, info);
 
         setDamage(DAMAGE, UPG_DAMAGE);
 
-        tags.add(CardTags.STRIKE);
         DamageModifierManager.addModifier(this, new MagicAttack());
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        this.baseMagicNumber = 0;
-
-        for(AbstractOrb o : AbstractDungeon.actionManager.orbsChanneledThisCombat) {
-            if (o instanceof Aether) {
-                ++this.baseMagicNumber;
-            }
-        }
-
-        this.magicNumber = this.baseMagicNumber;
-
-        for (int i = 0; i < this.magicNumber; i++) {
-            this.addToBot(new AttackDamageRandomEnemyAction(this, AbstractGameAction.AttackEffect.FIRE));
-        }
+        addToBot(new LoseHPAction(p, p, 1));
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.LIGHTNING));
+        addToBot(new BloodAction(m, p, new MakeTempCardInDrawPileAction(this.makeStatEquivalentCopy(), 1, false, true)));
     }
 
     @Override
