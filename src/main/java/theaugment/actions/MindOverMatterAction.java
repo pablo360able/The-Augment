38cf2a0f79f771902647ec76dff01d1bf4de78f6
pablo360able <pablo360.repiuk@gmail.com>
2025-10-20 10:12:42 -0400

    Added Mind over Matter

---
+++ src/main/java/theaugment/actions/MindOverMatterAction.java
@@ -0,0 +1,34 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package theaugment.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.FocusPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+public class MindOverMatterAction extends AbstractGameAction {
+    private AbstractPlayer p;
+
+    public MindOverMatterAction() {
+        this.actionType = ActionType.WAIT;
+        this.p = AbstractDungeon.player;
+        this.duration = Settings.ACTION_DUR_XFAST;
+    }
+
+    public void update() {
+        if (this.duration == Settings.ACTION_DUR_XFAST && this.p.hasPower("Strength")) {
+            int strAmt = this.p.getPower("Strength").amount;
+            this.addToTop(new ApplyPowerAction(this.p, this.p, new FocusPower(this.p, strAmt), strAmt));
+            this.addToTop(new ApplyPowerAction(this.p, this.p, new StrengthPower(this.p, -strAmt), -strAmt));
+        }
+
+        this.tickDuration();
+    }
+}
