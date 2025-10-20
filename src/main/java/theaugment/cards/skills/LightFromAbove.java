4c1dc1d91e93f00d94f4b210b5a2d75a9b33c464
pablo360able <pablo360.repiuk@gmail.com>
2025-10-20 14:23:20 -0400

    Added Light from Above

---
+++ src/main/java/theaugment/cards/skills/LightFromAbove.java
@@ -0,0 +1,40 @@
+package theaugment.cards.skills;
+
+import com.megacrit.cardcrawl.actions.defect.ChannelAction;
+import com.megacrit.cardcrawl.actions.unique.ArmamentsAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theaugment.cards.BaseCard;
+import theaugment.cards.CustomTags;
+import theaugment.character.Augment;
+import theaugment.orbs.Aether;
+import theaugment.util.CardStats;
+
+public class LightFromAbove extends BaseCard {
+    public static final String ID = makeID(LightFromAbove.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            Augment.Meta.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.UNCOMMON,
+            CardTarget.SELF,
+            1
+    );
+
+    public LightFromAbove() {
+        super(ID, info);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ArmamentsAction(this.upgraded));
+        if (p.hasEmptyOrb()) {
+            addToBot(new ChannelAction(new Aether()));
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new LightFromAbove();
+    }
+}
