7bc1b0d5f6103ff02704a9bc07bc226a49b4885c
pablo360able <pablo360.repiuk@gmail.com>
2025-10-20 12:50:32 -0400

    Added Realize

---
+++ src/main/java/theaugment/cards/skills/Realize.java
@@ -0,0 +1,44 @@
+package theaugment.cards.skills;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.defect.IncreaseMaxOrbAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.FocusPower;
+import theaugment.cards.BaseCard;
+import theaugment.character.Augment;
+import theaugment.util.CardStats;
+
+public class Realize extends BaseCard {
+    public static final String ID = makeID(Realize.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            Augment.Meta.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.UNCOMMON,
+            CardTarget.SELF,
+            1
+    );
+
+    public Realize() {
+        super(ID, info);
+    }
+
+    @Override
+    public void upgrade() {
+        this.baseCost = 0;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new FocusPower(p, -1)));
+        addToBot(new IncreaseMaxOrbAction(1));
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Realize();
+    }
+}
