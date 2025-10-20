0f66bdbddbd8dcbc7c6419f9ef4bf611a16872e8
pablo360able <pablo360.repiuk@gmail.com>
2025-10-20 10:19:52 -0400

    Added Disengage

---
+++ src/main/java/theaugment/cards/Disengage.java
@@ -0,0 +1,46 @@
+package theaugment.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theaugment.character.Augment;
+import theaugment.util.CardStats;
+
+public class Disengage extends BaseCard {
+    public static final String ID = makeID(Disengage.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            Augment.Meta.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.COMMON,
+            CardTarget.SELF,
+            1
+    );
+    private static final int BLOCK = 5;
+    private static final int UPG_BLOCK = 3;
+
+    public Disengage() {
+        super(ID, info);
+
+        setBlock(BLOCK, UPG_BLOCK);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainBlockAction(p, block));
+
+        for(AbstractMonster m2 : AbstractDungeon.getCurrRoom().monsters.monsters) {
+            if (!m2.isDeadOrEscaped()) {
+                addToBot(new GainBlockAction(p, block));            }
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Disengage();
+    }
+}
