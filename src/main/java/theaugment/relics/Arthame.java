f21d6562acf4a6bd5b4587d5152f3140cf7bf6e2
pablo360able <pablo360.repiuk@gmail.com>
2025-10-21 11:33:23 -0400

    Added Arthame

---
+++ src/main/java/theaugment/relics/Arthame.java
@@ -0,0 +1,38 @@
+package theaugment.relics;
+
+
+import com.evacipated.cardcrawl.mod.stslib.damagemods.AbstractDamageModifier;
+import com.evacipated.cardcrawl.mod.stslib.damagemods.DamageModifierManager;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import theaugment.character.Augment;
+import theaugment.modifiers.MagicAttack;
+import theaugment.util.Helpers;
+
+import static theaugment.TheAugmentMod.makeID;
+
+public class Arthame extends BaseRelic {
+    private static final String NAME = Arthame.class.getSimpleName();
+    public static final String ID = makeID(NAME);
+    private static final RelicTier RARITY = RelicTier.UNCOMMON;
+    private static final LandingSound SOUND = LandingSound.CLINK;
+
+    public Arthame() {
+        super(ID, NAME, Augment.Meta.CARD_COLOR, RARITY, SOUND);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return this.DESCRIPTIONS[0];
+    }
+
+    @Override
+    public float atDamageModify(float damage, AbstractCard card) {
+        //If NORMAL (attack) damage that isn't already magic, apply magic damage
+        for (AbstractDamageModifier mod : DamageModifierManager.modifiers(card)) {
+            if (mod instanceof MagicAttack) {
+                return damage;
+            }
+        }
+        return Helpers.EnchantDamage(damage, card.damageTypeForTurn);
+    }
+}
