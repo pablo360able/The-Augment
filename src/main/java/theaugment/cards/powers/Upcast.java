4932328d53ea10955f51b69fcb911a8e90cafd65
pablo360able <pablo360.repiuk@gmail.com>
2025-10-20 22:07:59 -0400

    Added Upcast

---
+++ src/main/java/theaugment/cards/powers/Upcast.java
@@ -0,0 +1,40 @@
+package theaugment.cards.powers;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.FocusPower;
+import theaugment.cards.BaseCard;
+import theaugment.cards.CustomTags;
+import theaugment.character.Augment;
+import theaugment.util.CardStats;
+
+public class Upcast extends BaseCard {
+    public static final String ID = makeID(Upcast.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            Augment.Meta.CARD_COLOR,
+            CardType.POWER,
+            CardRarity.UNCOMMON,
+            CardTarget.SELF,
+            2
+    );
+    private static int MAGIC = 3;
+    private static int UPG_MAGIC = 2;
+
+    public Upcast() {
+        super(ID, info);
+
+        setMagic(MAGIC, UPG_MAGIC);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new FocusPower(p, magicNumber)));
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Upcast();
+    }
+}
