2c879bcf7453a33a73bdc6ae2d0c6d9585707ef8
pablo360able <pablo360.repiuk@gmail.com>
2025-10-21 10:58:19 -0400

    Added Phurba

---
+++ src/main/java/theaugment/relics/Phurba.java
@@ -0,0 +1,52 @@
+package theaugment.relics;
+
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.FocusPower;
+import theaugment.character.Augment;
+
+import static theaugment.TheAugmentMod.makeID;
+
+public class Phurba extends BaseRelic {
+    private static final String NAME = Phurba.class.getSimpleName();
+    public static final String ID = makeID(NAME);
+    private static final RelicTier RARITY = RelicTier.SHOP;
+    private static final LandingSound SOUND = LandingSound.CLINK;
+
+    public Phurba() {
+        super(ID, NAME, Augment.Meta.CARD_COLOR, RARITY, SOUND);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return this.DESCRIPTIONS[0] + 3 + this.DESCRIPTIONS[1] + 1 + this.DESCRIPTIONS[2];
+    }
+
+    @Override
+    public void atTurnStart() {
+        this.counter = 0;
+    }
+
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (card.type == AbstractCard.CardType.ATTACK) {
+            ++this.counter;
+            if (this.counter % 3 == 0) {
+                this.counter = 0;
+                this.flash();
+                this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+                this.addToBot(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new FocusPower(AbstractDungeon.player, 1), 1));
+            }
+        }
+
+    }
+
+    @Override
+    public void onVictory() {
+        this.counter = -1;
+    }
+}
