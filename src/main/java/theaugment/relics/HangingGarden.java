7be2b0a6d67934fe8e271dfb2cd27c489f260a98
pablo360able <pablo360.repiuk@gmail.com>
2025-10-21 12:32:59 -0400

    Added Hanging Garden

---
+++ src/main/java/theaugment/relics/HangingGarden.java
@@ -0,0 +1,72 @@
+package theaugment.relics;
+
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.FocusPower;
+import theaugment.character.Augment;
+
+import static theaugment.TheAugmentMod.makeID;
+
+public class HangingGarden extends BaseRelic {
+    private static final String NAME = HangingGarden.class.getSimpleName();
+    public static final String ID = makeID(NAME);
+    private static final RelicTier RARITY = RelicTier.RARE;
+    private static final LandingSound SOUND = LandingSound.CLINK;
+    private static final int FOCUS_AMT = 2;
+    private boolean firstTurn = true;
+
+    public HangingGarden() {
+        super(ID, NAME, Augment.Meta.CARD_COLOR, RARITY, SOUND);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return this.DESCRIPTIONS[0] + FOCUS_AMT + this.DESCRIPTIONS[1];
+    }
+
+    @Override
+    public void atTurnStart() {
+        if (this.firstTurn) {
+            if (this.counter == -2) {
+                this.pulse = false;
+                this.counter = -1;
+                this.flash();
+                this.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new FocusPower(AbstractDungeon.player, FOCUS_AMT)));
+                this.addToTop(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+            }
+
+            this.firstTurn = false;
+        }
+
+    }
+
+    @Override
+    public void atPreBattle() {
+        this.firstTurn = true;
+    }
+
+    @Override
+    public void setCounter(int counter) {
+        super.setCounter(counter);
+        if (counter == -2) {
+            this.pulse = true;
+        }
+
+    }
+
+    @Override
+    public void onEnterRestRoom() {
+        this.flash();
+        this.counter = -2;
+        this.pulse = true;
+    }
+
+    @Override
+    public boolean canSpawn() {
+        return Settings.isEndless || AbstractDungeon.floorNum <= 48;
+    }
+}
