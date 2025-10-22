24b7199e751128db6653f7828ea3e9fb4dc20984
pablo360able <pablo360.repiuk@gmail.com>
2025-10-22 15:05:19 -0400

    Added Stoneskin

---
+++ src/main/java/theaugment/cards/skills/Stoneskin.java
@@ -0,0 +1,48 @@
+package theaugment.cards.skills;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.EnergizedPower;
+import theaugment.cards.BaseCard;
+import theaugment.cards.CustomTags;
+import theaugment.character.Augment;
+import theaugment.powers.FatiguedPower;
+import theaugment.util.CardStats;
+
+public class Stoneskin extends BaseCard {
+    public static final String ID = makeID(Stoneskin.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            Augment.Meta.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.COMMON,
+            CardTarget.SELF,
+            1
+    );
+    private static final int BLOCK = 16;
+    private static final int UPG_BLOCK = 8;
+    private static final int MAGIC = 1;
+    private static final int UPG_MAGIC = 0;
+
+    public Stoneskin() {
+        super(ID, info);
+
+        setBlock(BLOCK, UPG_BLOCK);
+        setMagic(MAGIC, UPG_MAGIC);
+
+        tags.add(CustomTags.SPONTANEOUS);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainBlockAction(p, block));
+        addToBot(new ApplyPowerAction(p, p, new FatiguedPower(p, 1)));
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Stoneskin();
+    }
+}
---
+++ src/main/java/theaugment/powers/FatiguedPower.java
@@ -0,0 +1,56 @@
+package theaugment.powers;
+
+
+import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import theaugment.cards.attacks.MagicMissile;
+
+import static theaugment.TheAugmentMod.makeID;
+
+public class FatiguedPower extends BasePower {
+    public static final String POWER_ID = makeID(FatiguedPower.class.getSimpleName());
+    private static final PowerType TYPE = PowerType.DEBUFF;
+    private static final boolean TURN_BASED = true;
+
+    public FatiguedPower(AbstractCreature owner, int energyAmt) {
+        super(POWER_ID, TYPE, TURN_BASED, owner, energyAmt);
+        if (this.amount >= 999) {
+            this.amount = 999;
+        }
+        loadRegion("fasting");
+        this.img = null;
+        this.updateDescription();
+    }
+
+    @Override
+    public void stackPower(int stackAmount) {
+        super.stackPower(stackAmount);
+        if (this.amount >= 999) {
+            this.amount = 999;
+        }
+    }
+
+    @Override
+    public void onEnergyRecharge() {
+        this.flash();
+        AbstractDungeon.player.loseEnergy(1);
+        if (--this.amount == 0) {
+            this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, this));
+        } else {
+            this.updateDescription();
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        if (this.amount == 1) {
+            this.description = DESCRIPTIONS[0];
+        } else {
+            this.description = DESCRIPTIONS[1] + this.amount + DESCRIPTIONS[2];
+        }
+    }
+}
