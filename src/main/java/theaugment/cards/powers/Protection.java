a4765de3ac796c6012cdfc390f622e9f709e5ba9
pablo360able <pablo360.repiuk@gmail.com>
2025-10-22 12:20:00 -0400

    Added Protection

---
+++ src/main/java/theaugment/cards/powers/Protection.java
@@ -0,0 +1,43 @@
+package theaugment.cards.powers;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.ArtifactPower;
+import theaugment.cards.BaseCard;
+import theaugment.character.Augment;
+import theaugment.powers.LoseArtifactPower;
+import theaugment.util.CardStats;
+
+public class Protection extends BaseCard {
+    public static final String ID = makeID(Protection.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            Augment.Meta.CARD_COLOR,
+            CardType.POWER,
+            CardRarity.UNCOMMON,
+            CardTarget.SELF,
+            1
+    );
+    private static final int MAGIC = 2;
+    private static final int UPG_MAGIC = 0;
+
+    public Protection() {
+        super(ID, info);
+
+        setMagic(MAGIC, UPG_MAGIC);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (!upgraded) {
+            addToBot(new ApplyPowerAction(p, p, new LoseArtifactPower(p, magicNumber)));
+        }
+        addToBot(new ApplyPowerAction(p, p, new ArtifactPower(p, magicNumber)));
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Protection();
+    }
+}
---
+++ src/main/java/theaugment/powers/LoseArtifactPower.java
@@ -0,0 +1,40 @@
+package theaugment.powers;
+
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import theaugment.util.GeneralUtils;
+
+import static theaugment.TheAugmentMod.makeID;
+
+public class LoseArtifactPower extends BasePower {
+    public static final String POWER_ID = makeID(LoseArtifactPower.class.getSimpleName());
+    private static final PowerType TYPE = PowerType.DEBUFF;
+    private static final boolean TURN_BASED = false;
+
+    public LoseArtifactPower(AbstractCreature owner, int newAmount) {
+        super(POWER_ID, TYPE, TURN_BASED, owner, newAmount);
+        String unPrefixed = GeneralUtils.removePrefix(POWER_ID);
+        this.updateDescription();
+        this.loadRegion("flex");
+        this.img = null;
+    }
+
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+    }
+
+    public void atEndOfTurn(boolean isPlayer) {
+        this.flash();
+        if (this.owner.hasPower("Artifact")) {
+            AbstractPower artifactPower = this.owner.getPower("Artifact");
+            if (artifactPower.amount <= this.amount) {
+                this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, artifactPower));
+            } else {
+                artifactPower.amount -= this.amount;
+                this.owner.updatePowers();
+            }
+        }
+        this.addToBot(new RemoveSpecificPowerAction(this.owner, this.owner, POWER_ID));
+    }
+}
