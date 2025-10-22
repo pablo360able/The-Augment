88fc1d55de409cc3c6a3e197e897a34bb10902a7
pablo360able <pablo360.repiuk@gmail.com>
2025-10-21 23:47:21 -0400

    Added Back Strike

---
+++ src/main/java/theaugment/actions/BackStrikeAction.java
@@ -0,0 +1,24 @@
+package theaugment.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class BackStrikeAction extends AbstractGameAction {
+    private AbstractMonster m;
+
+    public BackStrikeAction(AbstractMonster m) {
+        this.actionType = ActionType.WAIT;
+        this.m = m;
+    }
+
+    public void update() {
+        if (this.m != null && this.m.getPower("Frail") != null) {
+            this.addToTop(new GainEnergyAction(1));
+            this.addToTop(new DrawCardAction(1));
+        }
+
+        this.isDone = true;
+    }
+}
---
+++ src/main/java/theaugment/cards/attacks/BackStrike.java
@@ -0,0 +1,58 @@
+package theaugment.cards.attacks;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theaugment.actions.BackStrikeAction;
+import theaugment.actions.BlockNextTurnAction;
+import theaugment.actions.BloodAction;
+import theaugment.cards.BaseCard;
+import theaugment.character.Augment;
+import theaugment.util.CardStats;
+
+public class BackStrike extends BaseCard {
+    public static final String ID = makeID(BackStrike.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            Augment.Meta.CARD_COLOR,
+            CardType.ATTACK,
+            CardRarity.COMMON,
+            CardTarget.ENEMY,
+            1
+    );
+    private static final int DAMAGE = 9;
+    private static final int UPG_DAMAGE = 3;
+
+    public BackStrike() {
+        super(ID, info);
+
+        setDamage(DAMAGE, UPG_DAMAGE);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+        addToBot(new BackStrikeAction(m));
+    }
+
+    @Override
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+
+        for(AbstractMonster m : AbstractDungeon.getCurrRoom().monsters.monsters) {
+            if (!m.isDeadOrEscaped() && m.getPower("Frail") != null) {
+                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+                break;
+            }
+        }
+
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new BackStrike();
+    }
+}
