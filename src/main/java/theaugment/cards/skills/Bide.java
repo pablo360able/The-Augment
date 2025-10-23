c479202893ff38f334d01382d7ae4d8a635a88e1
pablo360able <pablo360.repiuk@gmail.com>
2025-10-22 22:58:35 -0400

    Added Bide, and glow to Unarmed Strike

--- src/main/java/theaugment/cards/skills/Bide.java
+++ src/main/java/theaugment/cards/skills/Bide.java
@@ -1,41 +1,39 @@
-package theaugment.cards.attacks;
+package theaugment.cards.skills;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.FocusPower;
 import theaugment.cards.BaseCard;
 import theaugment.character.Augment;
 import theaugment.util.CardStats;
 
-public class UnarmedStrike extends BaseCard {
-    public static final String ID = makeID(UnarmedStrike.class.getSimpleName());
+public class Bide extends BaseCard {
+    public static final String ID = makeID(Bide.class.getSimpleName());
     private static final CardStats info = new CardStats(
             Augment.Meta.CARD_COLOR,
-            CardType.ATTACK,
-            CardRarity.COMMON,
-            CardTarget.ENEMY,
+            CardType.SKILL,
+            CardRarity.UNCOMMON,
+            CardTarget.SELF_AND_ENEMY,
             1
     );
-    private static final int DAMAGE = 7;
-    private static final int UPG_DAMAGE = 2;
+    private static final int MAGIC = 2;
+    private static final int UPG_MAGIC = 0;
 
-    public UnarmedStrike() {
+    public Bide() {
         super(ID, info);
 
-        setDamage(DAMAGE, UPG_DAMAGE);
+        setMagic(MAGIC, UPG_MAGIC);
 
-        tags.add(CardTags.STRIKE);
+        setSelfRetain(false, true);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
         if (m.getIntentBaseDmg() < 0) {
-            addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+            addToBot(new ApplyPowerAction(p, p, new FocusPower(p, magicNumber)));
         }
     }
 
@@ -53,6 +51,6 @@ public class UnarmedStrike extends BaseCard {
 
     @Override
     public AbstractCard makeCopy() { //Optional
-        return new UnarmedStrike();
+        return new Bide();
     }
 }
