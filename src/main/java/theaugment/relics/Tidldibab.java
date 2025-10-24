83541ee7e6c028dc64985c983da1c680934e3b8b
pablo360able <pablo360.repiuk@gmail.com>
2025-10-24 14:29:15 -0400

    Added Tidldibab

---
+++ src/main/java/theaugment/relics/Tidldibab.java
@@ -0,0 +1,42 @@
+package theaugment.relics;
+
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import theaugment.character.Augment;
+import theaugment.powers.EntropyPower;
+
+import static theaugment.TheAugmentMod.makeID;
+
+public class Tidldibab extends BaseRelic {
+    private static final String NAME = Tidldibab.class.getSimpleName();
+    public static final String ID = makeID(NAME);
+    private static final RelicTier RARITY = RelicTier.BOSS;
+    private static final LandingSound SOUND = LandingSound.CLINK;
+
+    public Tidldibab() {
+        super(ID, NAME, Augment.Meta.CARD_COLOR, RARITY, SOUND);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return this.DESCRIPTIONS[0] + 1 + this.DESCRIPTIONS[1];
+    }
+
+    @Override
+    public void atTurnStartPostDraw() {
+        this.addToBot(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+        this.addToBot(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new EntropyPower(AbstractDungeon.player, 1)));
+    }
+
+    @Override
+    public void onEquip() {
+        ++AbstractDungeon.player.energy.energyMaster;
+    }
+
+    @Override
+    public void onUnequip() {
+        --AbstractDungeon.player.energy.energyMaster;
+    }
+}
