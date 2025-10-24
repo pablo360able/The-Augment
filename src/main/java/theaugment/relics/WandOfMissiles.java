5acb5e26fe301157f8cfdc0d8dcb98f48ed5405b
pablo360able <pablo360.repiuk@gmail.com>
2025-10-24 13:55:12 -0400

    Changed rarity for Wand of Missiles

--- src/main/java/theaugment/relics/WandOfMissiles.java
+++ src/main/java/theaugment/relics/WandOfMissiles.java
@@ -17,7 +17,7 @@ import static theaugment.TheAugmentMod.makeID;
 public class WandOfMissiles extends BaseRelic {
     private static final String NAME = WandOfMissiles.class.getSimpleName();
     public static final String ID = makeID(NAME);
-    private static final RelicTier RARITY = RelicTier.COMMON;
+    private static final RelicTier RARITY = RelicTier.UNCOMMON;
     private static final LandingSound SOUND = LandingSound.CLINK;
 
     public WandOfMissiles() {
