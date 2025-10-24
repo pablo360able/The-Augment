f73b58c4ea181ce83ec6977b5d3e06c11612990a
pablo360able <pablo360.repiuk@gmail.com>
2025-10-24 14:19:39 -0400

    Added Eddington's Arrow

---
+++ src/main/java/theaugment/relics/EddingtonsArrow.java
@@ -0,0 +1,37 @@
+package theaugment.relics;
+
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theaugment.character.Augment;
+import theaugment.powers.EntropyPower;
+
+import static theaugment.TheAugmentMod.makeID;
+
+public class EddingtonsArrow extends BaseRelic {
+    private static final String NAME = EddingtonsArrow.class.getSimpleName();
+    public static final String ID = makeID(NAME);
+    private static final RelicTier RARITY = RelicTier.COMMON;
+    private static final LandingSound SOUND = LandingSound.CLINK;
+
+    public EddingtonsArrow() {
+        super(ID, NAME, Augment.Meta.CARD_COLOR, RARITY, SOUND);
+    }
+
+    @Override
+    public String getUpdatedDescription() {
+        return this.DESCRIPTIONS[0] + 1 + this.DESCRIPTIONS[1];
+    }
+
+    @Override
+    public void atBattleStartPreDraw() {
+        this.flash();
+
+        for(AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
+            this.addToBot(new RelicAboveCreatureAction(mo, this));
+            this.addToBot(new ApplyPowerAction(mo, AbstractDungeon.player, new EntropyPower(mo, 1), 1, true));
+        }
+    }
+}
