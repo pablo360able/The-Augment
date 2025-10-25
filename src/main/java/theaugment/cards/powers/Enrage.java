d3a17f98241d96061bdb4e599a8aa4ebff8a8e54
pablo360able <pablo360.repiuk@gmail.com>
2025-10-25 15:33:27 -0400

    Added Enrage

---
+++ src/main/java/theaugment/cards/powers/Enrage.java
@@ -0,0 +1,51 @@
+package theaugment.cards.powers;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.FastDrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theaugment.cards.BaseCard;
+import theaugment.cards.CustomTags;
+import theaugment.character.Augment;
+import theaugment.powers.EnragePower;
+import theaugment.util.CardStats;
+
+import static theaugment.util.TextureLoader.getCardTextureString;
+
+public class Enrage extends BaseCard {
+    public static final String ID = makeID(Enrage.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            Augment.Meta.CARD_COLOR,
+            CardType.POWER,
+            CardRarity.UNCOMMON,
+            CardTarget.SELF,
+            2
+    );
+    private static final int MAGIC = 2;
+    private static final int UPG_MAGIC = 1;
+
+    public Enrage() {
+        super(ID, info);
+
+        setMagic(MAGIC, UPG_MAGIC);
+
+        this.tags.add(CustomTags.SPONTANEOUS);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new EnragePower(magicNumber, getCardTextureString(ID, info.cardType))));
+    }
+
+    @Override
+    public void triggerOnManualDiscard() {
+        addToTop(new FastDrawCardAction(AbstractDungeon.player, 1));
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Enrage();
+    }
+}
---
+++ src/main/java/theaugment/powers/EnragePower.java
@@ -0,0 +1,43 @@
+
+package theaugment.powers;
+
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+import static theaugment.TheAugmentMod.makeID;
+import static theaugment.util.Helpers.IntentContains;
+
+public class EnragePower extends ConcentrationPower {
+    public static final String POWER_ID = makeID(EnragePower.class.getSimpleName());
+    private static final PowerType TYPE = PowerType.BUFF;
+    private static final boolean TURN_BASED = false;
+
+    public EnragePower(int amount, String cardImage) {
+        super(POWER_ID, TYPE, TURN_BASED, amount, cardImage);
+        loadRegion("anger");
+        this.img = null;
+        this.updateDescription();
+    }
+
+    @Override
+    public void atStartOfTurnPostDraw() {
+        int skilling = 0;
+        for (AbstractMonster m : AbstractDungeon.getCurrRoom().monsters.monsters) {
+            if (IntentContains(m, AbstractMonster.Intent.DEBUFF) && !IntentContains(m, AbstractMonster.Intent.ATTACK) || IntentContains(m, AbstractMonster.Intent.DEFEND)) {
+                skilling++;
+            }
+        }
+        if (skilling > 0) {
+            this.flash();
+            this.addToBot(new ApplyPowerAction(this.owner, this.owner, new StrengthPower(this.owner, this.amount * skilling)));
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+    }
+}
