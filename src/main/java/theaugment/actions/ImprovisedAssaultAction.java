4171385f6001072a976f0f18034f118b9ab562c9
pablo360able <pablo360.repiuk@gmail.com>
2025-10-26 17:35:46 -0400

    Added Improvised Assault

---
+++ src/main/java/theaugment/actions/ImprovisedAssaultAction.java
@@ -0,0 +1,66 @@
+package theaugment.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import theaugment.cards.attacks.MagicMissile;
+
+public class ImprovisedAssaultAction extends AbstractGameAction {
+    private static final UIStrings uiStrings;
+    public static final String[] TEXT;
+    private final AbstractPlayer p;
+    private final boolean upgraded;
+    private int replaced;
+
+    public ImprovisedAssaultAction(boolean upgraded) {
+        this.p = AbstractDungeon.player;
+        this.replaced = 0;
+        this.upgraded = upgraded;
+        this.duration = this.startDuration = Settings.ACTION_DUR_FAST;
+        this.actionType = ActionType.EXHAUST;
+    }
+
+    public void update() {
+        if (this.duration == this.startDuration) {
+            if (this.p.hand.isEmpty()) {
+                this.isDone = true;
+                return;
+            }
+
+            AbstractDungeon.handCardSelectScreen.open(TEXT[0], this.p.hand.size(), true, true);
+            this.tickDuration();
+            return;
+        }
+
+        if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
+            CardGroup temp = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
+            for(AbstractCard c : AbstractDungeon.handCardSelectScreen.selectedCards.group) {
+                temp.addToRandomSpot(c);
+                this.replaced++;
+            }
+            for (AbstractCard c : temp.group) {
+                this.p.hand.moveToDeck(c, false);
+                MagicMissile mm = new MagicMissile();
+                if (this.upgraded) {
+                    mm.upgrade();
+                }
+                this.addToTop(new MakeTempCardInHandAction(mm));
+            }
+
+            AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+        }
+
+        this.tickDuration();
+    }
+
+    static {
+        uiStrings = CardCrawlGame.languagePack.getUIString("SetupAction");
+        TEXT = uiStrings.TEXT;
+    }
+}
--- src/main/java/theaugment/cards/skills/ImprovisedAssault.java
+++ src/main/java/theaugment/cards/skills/ImprovisedAssault.java
@@ -1,43 +1,38 @@
 package theaugment.cards.skills;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theaugment.actions.ImprovisedAssaultAction;
 import theaugment.actions.ReconsiderAction;
 import theaugment.cards.BaseCard;
 import theaugment.character.Augment;
-import theaugment.powers.MirrorImagePower;
 import theaugment.util.CardStats;
 
-public class Reconsider extends BaseCard {
-    public static final String ID = makeID(Reconsider.class.getSimpleName());
+public class ImprovisedAssault extends BaseCard {
+    public static final String ID = makeID(ImprovisedAssault.class.getSimpleName());
     private static final CardStats info = new CardStats(
             Augment.Meta.CARD_COLOR,
             CardType.SKILL,
-            CardRarity.UNCOMMON,
+            CardRarity.RARE,
             CardTarget.SELF,
             1
     );
-    private static final int MAGIC = 2;
-    private static final int UPG_MAGIC = 1;
 
-    public Reconsider() {
+    public ImprovisedAssault() {
         super(ID, info);
 
-        setMagic(MAGIC, UPG_MAGIC);
+        setExhaust(true);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ReconsiderAction(upgraded));
-        addToBot(new DrawCardAction(magicNumber));
+        addToBot(new ImprovisedAssaultAction(upgraded));
     }
 
     @Override
     public AbstractCard makeCopy() { //Optional
-        return new Reconsider();
+        return new ImprovisedAssault();
     }
 }
