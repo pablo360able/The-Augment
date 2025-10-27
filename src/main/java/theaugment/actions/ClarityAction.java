c40d6f427c9c20d055e370014d10180371b468f5
pablo360able <pablo360.repiuk@gmail.com>
2025-10-26 20:28:11 -0400

    Added Clarity

---
+++ src/main/java/theaugment/actions/ClarityAction.java
@@ -0,0 +1,70 @@
+package theaugment.actions;
+
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import com.megacrit.cardcrawl.powers.FocusPower;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+
+import static theaugment.TheAugmentMod.makeID;
+
+public class ClarityAction extends AbstractGameAction {
+    private static final UIStrings uiStrings;
+    public static final String[] TEXT;
+    private final AbstractPlayer p;
+
+    public ClarityAction() {
+        this.actionType = ActionType.CARD_MANIPULATION;
+        this.p = AbstractDungeon.player;
+        this.duration = Settings.ACTION_DUR_FAST;
+    }
+
+    public void update() {
+        if (this.duration == Settings.ACTION_DUR_FAST) {
+            if (this.p.hand.isEmpty()) {
+                this.isDone = true;
+            } else if (this.p.hand.size() == 1) {
+                AbstractCard c = this.p.hand.getBottomCard();
+                if (c.costForTurn > 0) {
+                    c.cost = 0;
+                    c.costForTurn = 0;
+                    c.isCostModified = true;
+                    c.superFlash(Color.GOLD.cpy());
+                    this.p.hand.addToTop(c);
+                }
+                this.tickDuration();
+            } else {
+                AbstractDungeon.handCardSelectScreen.open(TEXT[0], 1, false);
+                this.tickDuration();
+            }
+        } else {
+            if (!AbstractDungeon.handCardSelectScreen.wereCardsRetrieved) {
+                for(AbstractCard c : AbstractDungeon.handCardSelectScreen.selectedCards.group) {
+                    if (c.costForTurn > 0) {
+                        c.cost = 0;
+                        c.costForTurn = 0;
+                        c.isCostModified = true;
+                        c.superFlash(Color.GOLD.cpy());
+                        this.p.hand.addToTop(c);
+                    }
+                }
+
+                AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true;
+                AbstractDungeon.handCardSelectScreen.selectedCards.group.clear();
+            }
+
+            this.tickDuration();
+        }
+    }
+
+    static {
+        uiStrings = CardCrawlGame.languagePack.getUIString(makeID(EfficiencyAction.class.getSimpleName()));
+        TEXT = uiStrings.TEXT;
+    }
+}
---
+++ src/main/java/theaugment/cards/skills/Clarity.java
@@ -0,0 +1,50 @@
+package theaugment.cards.skills;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.unique.ExpertiseAction;
+import com.megacrit.cardcrawl.actions.utility.ScryAction;
+import com.megacrit.cardcrawl.actions.watcher.PressEndTurnButtonAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.FocusPower;
+import theaugment.actions.ClarityAction;
+import theaugment.actions.ReshuffleSpecificCardAction;
+import theaugment.cards.BaseCard;
+import theaugment.cards.CustomTags;
+import theaugment.character.Augment;
+import theaugment.util.CardStats;
+
+public class Clarity extends BaseCard {
+    public static final String ID = makeID(Clarity.class.getSimpleName());
+    private static final CardStats info = new CardStats(
+            Augment.Meta.CARD_COLOR,
+            CardType.SKILL,
+            CardRarity.RARE,
+            CardTarget.SELF,
+            2
+    );
+
+    public Clarity() {
+        super(ID, info);
+
+        tags.add(CustomTags.SPONTANEOUS);
+        setExhaust(true);
+        setCostUpgrade(1);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new FocusPower(p, -1)));
+        addToBot(new ClarityAction());
+        if (!p.hasPower("Focus") || p.getPower("Focus").amount <= 1) {
+            addToBot(new PressEndTurnButtonAction());
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new Clarity();
+    }
+}
